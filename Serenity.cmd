8e0b18259aff8b0fd08d9b0d8df69031a7ecd69e
paulparkercmh <pdp@computer.org>
2023-04-07 16:52:21 -0400

    Add files via upload

---
+++ Serenity.cmd
@@ -0,0 +1,9 @@
+#Serenity Cucumber Examples
+
+git clone  -b screenplay https://github.com/serenity-bdd/serenity-cucumber-starter.git screenplay
+git clone  -b tutorials https://github.com/serenity-bdd/serenity-cucumber-starter.git tutorials
+git clone  -b gh-pages https://github.com/serenity-bdd/serenity-cucumber-starter.git gh-pages
+git clone  -b action-classes https://github.com/serenity-bdd/serenity-cucumber-starter.git action-classes
+git clone  -b master https://github.com/serenity-bdd/serenity-cucumber-starter.git master
+
+#Serenity
